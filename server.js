ce82f90152eaf9f7c5cf18b7cab3c4f30d4e2b30
01-akm <kraadi4941@gmail.com>
2025-09-02 11:26:51 +0530

    the updated server.js file. I've added the logic to listen for the new 'private message' event, find the correct recipient, and send the message only to them and the original sender.

--- server.js
+++ server.js
@@ -1,41 +1,62 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require("socket.io");
-
 const app = express();
+const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
 const io = new Server(server);
+const path = require('path');
 
-const PORT = 3000;
-const users = {}; // Store users: { socketId: 'username' }
+const PORT = process.env.PORT || 3000;
 
-// Serve the 'public' folder as static files
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Listen for new connections
+// Store users: key is socket.id, value is username
+const users = {}; 
+
 io.on('connection', (socket) => {
-    console.log('A user has connected:', socket.id);
+    console.log('A user has connected...');
 
-    // Listen for username setup
     socket.on('set username', (username) => {
         socket.username = username;
         users[socket.id] = username;
-        // Broadcast the updated user list to everyone
         io.emit('update user list', Object.values(users));
     });
 
-    // Listen for chat messages
-    socket.on('chat message', (msg) => {
-        // Broadcast the message to everyone
-        io.emit('chat message', { user: socket.username, text: msg.text });
+    socket.on('chat message', (data) => {
+        io.emit('chat message', { user: socket.username, text: data.text });
+    });
+    
+    // --- New handler for private messages ---
+    socket.on('private message', (data) => {
+        const recipientUsername = data.to;
+        let recipientSocketId = null;
+
+        // Find the recipient's socket ID from their username
+        for (const id in users) {
+            if (users[id] === recipientUsername) {
+                recipientSocketId = id;
+                break;
+            }
+        }
+
+        // If the recipient is found, send the message
+        if (recipientSocketId) {
+            const messageData = {
+                text: data.text,
+                from: socket.username,
+            };
+            // Send to the recipient
+            io.to(recipientSocketId).emit('private message', messageData);
+            // Also send back to the sender so they can see it
+            socket.emit('private message', messageData);
+        }
     });
 
-    // Listen for file uploads
+
     socket.on('upload file', (fileData) => {
         io.emit('file message', { user: socket.username, file: fileData });
     });
 
-    // Listen for typing events
     socket.on('typing', () => {
         socket.broadcast.emit('typing', socket.username);
     });
@@ -46,38 +67,45 @@ io.on('connection', (socket) => {
 
     // --- WebRTC Signaling ---
     socket.on('call user', (data) => {
-        const userToCall = Object.keys(users).find(key => users[key] === data.userToCall);
-        if (userToCall) {
-            io.to(userToCall).emit('call received', {
+        let recipientSocketId = null;
+        for (const id in users) {
+            if (users[id] === data.userToCall) {
+                recipientSocketId = id;
+                break;
+            }
+        }
+        if (recipientSocketId) {
+            io.to(recipientSocketId).emit("call received", {
                 signal: data.signalData,
                 from: { id: socket.id, username: socket.username }
             });
         }
     });
 
-    socket.on('answer call', (data) => {
+    socket.on("answer call", (data) => {
         io.to(data.to).emit('call answered', data.signal);
     });
 
-    // Listen for hang up event and relay it
-    socket.on('hang up', (data) => {
-        const userToNotify = Object.keys(users).find(key => users[key] === data.user);
-        if (userToNotify) {
-            io.to(userToNotify).emit('call ended');
+    socket.on("hang up", (data) => {
+        let recipientSocketId = null;
+        for (const id in users) {
+            if (users[id] === data.user) {
+                recipientSocketId = id;
+                break;
+            }
+        }
+        if (recipientSocketId) {
+            io.to(recipientSocketId).emit('call ended');
         }
     });
-
-    // Listen for disconnections
+    
     socket.on('disconnect', () => {
-        console.log('A user has disconnected:', socket.id);
-        // You might want to also signal a hang up if the user was in a call
+        console.log('A user has disconnected...');
         delete users[socket.id];
-        // Broadcast the updated user list to everyone
         io.emit('update user list', Object.values(users));
     });
 });
 
-// Start the server
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
